c5d814f97eeb90a1056a4d9456ce189904f69f5d
Joel-Halkey <98487188+Joel-Halkey@users.noreply.github.com>
2023-03-29 15:36:35 -0400

    Added JUnit 5

--- src/main/java/com/calcyoulater/working/App.java
+++ src/main/java/com/calcyoulater/working/App.java
@@ -1,13 +1,11 @@
 package com.calcyoulater.working;
 
-/**
- * Hello world!
- *
- */
+import com.calcyoulater.gui.Calculator;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculator c = new Calculator();
     }
 }
--- src/test/java/com/calcyoulater/AppTest.java
+++ src/test/java/com/calcyoulater/AppTest.java
@@ -1,10 +1,8 @@
 package com.calcyoulater;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
