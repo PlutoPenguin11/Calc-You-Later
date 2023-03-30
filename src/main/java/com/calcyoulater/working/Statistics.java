f67540a5bf18af45efe2ebe6a9a2c8ea5113dd5e
PlutoPenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2023-03-29 23:03:20 -0400

    Added statistics
    Added Will's stats class and test cases associated

--- src/main/java/com/calcyoulater/working/Statistics.java
+++ src/main/java/com/calcyoulater/working/Statistics.java
@@ -1,5 +1,148 @@
 package com.calcyoulater.working;
 
 public class Statistics {
-    
+    public static double mean(double[] input) {
+        double sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i];
+        }
+        return (sum / input.length);
+    }
+
+    public static double minimum(double[] input) {
+        double minimum = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] < minimum) {
+                minimum = input[i];
+            }
+        }
+        return minimum;
+    }
+
+    public static double maximum(double[] input) {
+        double maximum = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] > maximum) {
+                maximum = input[i];
+            }
+        }
+        return maximum;
+    }
+
+    public static double[] quartiles(double[] input) {
+        quickSort(input, 0, input.length - 1);
+        double min, q1, med, q3, max;
+        min = input[0];
+        max = input[input.length - 1];
+        int lower = input.length / 2;
+        int extra = input.length % 2;
+        if (extra == 1) {
+            med = input[lower];
+        } else {
+            med = (input[lower] + input[lower - 1]) / 2;
+        }
+        q1 = input[lower / 2];
+        q3 = input[lower + (lower / 2)];
+        double[] result = { min, q1, med, q3, max };
+        return result;
+
+    }
+
+    public static double median(double[] input) {
+        quickSort(input, 0, input.length - 1);
+        double med;
+        int lower = input.length / 2;
+        int extra = input.length % 2;
+        if (extra == 1) {
+            med = input[lower];
+        } else {
+            med = (input[lower] + input[lower - 1]) / 2;
+        }
+        return med;
+    }
+
+    public static double meanSquared(double[] input) {
+        double x = mean(input);
+        return x * x;
+    }
+
+    public static double meanOfSquares(double[] input) {
+        double sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i] * input[i];
+        }
+        return sum / input.length;
+    }
+
+    public static int count(double[] input) {
+        return input.length;
+    }
+
+    public static double sum(double[] input) {
+        double sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i];
+        }
+        return sum;
+    }
+
+    public static double variance(double[] input) {
+        double a = meanSquared(input);
+        double b = meanOfSquares(input);
+        return b - a;
+    }
+
+    public static double stddev(double[] input) {
+        return Math.sqrt(variance(input));
+    }
+
+    public static double stderror(double[] input) {
+        double factor = Math.sqrt((double) (count(input) + 1) / (double) count(input));
+        return factor * stddev(input);
+    }
+
+    public static double tstat(double[] input, double target) {
+        double error = stderror(input);
+        double mean = mean(input);
+        return (mean - target) / error;
+    }
+
+    public static boolean isSignificant(double[] input, double target, double critical) {
+        double tstat = tstat(input, target);
+        if (critical > 0) {
+            return (tstat > critical);
+        } else {
+            return (tstat < critical);
+        }
+    }
+
+    private static void quickSort(double arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(double arr[], int begin, int end) {
+        double pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                double swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        double swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
+    }
 }
---
+++ src/test/java/com/calcyoulater/StatisticsTest.java
@@ -0,0 +1,130 @@
+package com.calcyoulater;
+
+/*import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;*/
+
+import com.calcyoulater.working.Statistics;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsTest {
+
+    private double[] testArray;
+    private double temp;
+
+    @BeforeEach
+    public void setUp() {
+        testArray = new double[] { 0, 471, 727, 3.14, 123 };
+    }
+
+    @Test
+    public void testMean() {
+        temp = Statistics.mean(testArray);
+        assertTrue(Math.abs(temp - 264.828) < 0.01);
+    }
+
+    @Test
+    public void testMinimum() {
+        temp = Statistics.minimum(testArray);
+        assertTrue(Math.abs(temp - 0) < 0.01);
+
+    }
+
+    @Test
+    public void testMaximum() {
+        temp = Statistics.maximum(testArray);
+        assertTrue(Math.abs(temp - 727) < 0.01);
+
+    }
+
+    @Test
+    public void testQuartiles() {
+        double[] temp = Statistics.quartiles(testArray);
+        double[] quartiles = { 0, 3.14, 123, 471, 727 };
+        boolean flag = true;
+        double countoff = 0;
+        for (int i = 0; i < 5; i++) {
+            if (Math.abs(temp[i] - quartiles[i]) > 0.01) {
+                flag = false;
+                countoff = temp[i];
+
+            }
+        }
+        assertTrue(flag);
+    }
+
+    @Test
+    public void testMedian() {
+        temp = Statistics.median(testArray);
+        assertTrue(Math.abs(temp - 123) < 0.01);
+
+    }
+
+    @Test
+    public void testMeanSquared() {
+        temp = Statistics.meanSquared(testArray);
+        assertTrue(Math.abs(temp - 70133.8696) < 0.01);
+
+    }
+
+    @Test
+    public void testSquareofMeans() {
+        temp = Statistics.meanOfSquares(testArray);
+        assertTrue(Math.abs(temp - 153101.77192) < 0.01);
+
+    }
+
+    @Test
+    public void testCount() {
+        int temp = Statistics.count(testArray);
+        assertTrue(temp == 5);
+    }
+
+    @Test
+    public void testSum() {
+        temp = Statistics.sum(testArray);
+        assertTrue(Math.abs(temp - 264.828 * 5) < 0.01);
+    }
+
+    @Test
+    public void testVar() {
+        temp = Statistics.variance(testArray);
+        assertTrue(Math.abs(temp - 82967.9023) < 0.01);
+
+    }
+
+    @Test
+    public void testDev() {
+        temp = Statistics.stddev(testArray);
+        assertTrue(Math.abs(temp - 288.041494) < 0.01);
+    }
+
+    @Test
+    public void testErr() {
+        temp = Statistics.stderror(testArray);
+        assertTrue(Math.abs(temp - 315.5336476561572) < 0.01);
+
+    }
+
+    @Test
+    public void testTstat() {
+        temp = Statistics.tstat(testArray, 0);
+        assertTrue(Math.abs(temp - 0.8393019317185086) < 0.01);
+    }
+
+    @Test
+    public void testIsSignificant() {
+        boolean temp = Statistics.isSignificant(testArray, 0, 0.5);
+        assertTrue(temp);
+    }
+
+    @Test
+    public void testIsSignificant2() {
+        boolean temp = Statistics.isSignificant(testArray, 0, 1.5);
+        assertTrue(!temp);
+    }
+}
