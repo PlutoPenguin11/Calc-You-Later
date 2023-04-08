890d58d39a7914233150329343011900efb5ef55
Jonathon <jonathon.esbri@gmail.com>
2023-04-08 12:25:13 -0400

    grapher class

--- src/main/java/com/calcyoulater/working/App.java
+++ src/main/java/com/calcyoulater/working/App.java
@@ -1,11 +1,12 @@
 package com.calcyoulater.working;
 
 import com.calcyoulater.gui.Calculator;
-
+import com.calcyoulater.working.Grapher;
 public class App 
 {
     public static void main( String[] args )
     {
         Calculator c = new Calculator();
+       
     }
 }
