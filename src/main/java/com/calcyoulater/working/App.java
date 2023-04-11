e5b881e7c9186d362849aede29c14d17997dd672
Joel-Halkey <98487188+Joel-Halkey@users.noreply.github.com>
2023-04-10 21:04:38 -0400

    Removed contents of grapher to be able to copy paste

--- src/main/java/com/calcyoulater/working/App.java
+++ src/main/java/com/calcyoulater/working/App.java
@@ -1,7 +1,6 @@
 package com.calcyoulater.working;
 
 import com.calcyoulater.gui.Calculator;
-import com.calcyoulater.working.Grapher;
 
 public class App {
     public static void main(String[] args) {
