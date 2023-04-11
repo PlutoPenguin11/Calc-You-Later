bdc5ad1239afc8ccb616c5d3841aeabed8f8ff34
Joel-Halkey <98487188+Joel-Halkey@users.noreply.github.com>
2023-04-10 21:45:37 -0400

    Added icon to app

--- src/main/java/com/calcyoulater/working/App.java
+++ src/main/java/com/calcyoulater/working/App.java
@@ -1,9 +1,22 @@
 package com.calcyoulater.working;
 
+import java.awt.Image;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
 import com.calcyoulater.gui.Calculator;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Calculator c = Calculator.instance();
+        App.setIconImage(c,"/calculator_icon.png");
+    }
+
+    public static void setIconImage(Calculator frame, String imageName) throws IOException {
+        InputStream is = App.class.getResourceAsStream(imageName);
+        Image image = ImageIO.read(is);
+        frame.setIconImage(image);
     }
 }
